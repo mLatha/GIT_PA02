45099a05a9355ae6fa80b83221986e5940734f09
derpy83 <64124288+derpy83@users.noreply.github.com>
2020-07-22 11:43:48 +0800

    .

---
+++ Assets/Scripts/ObstacleSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawn : MonoBehaviour
+{
+    public GameObject[] Obstacles;
+
+    private float spawnDelay = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Obstacles = Random.Range(1, 4);
+        InvokeRepeating("Spawn", spawnDelay, spawnDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Spawn()
+    {
+        var O = GameObject.Instantiate(Obstacles);
+    }
+}
